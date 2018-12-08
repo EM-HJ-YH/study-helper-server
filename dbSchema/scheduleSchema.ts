3eba5a6c9c12011d31ab3fba7b8cc1d78dd969fc
YongHoonJJo <starctak@gmail.com>
2018-12-08 20:14:53 +0900

    chage Year, Month, Day type :: Number > String

--- dbSchema/scheduleSchema.ts
+++ dbSchema/scheduleSchema.ts
@@ -2,9 +2,9 @@ import * as mongoose from 'mongoose';
 
 const scheduleSchema: any = mongoose.Schema({
     scheduleIndex: { type: Number, required: true, unique: true},
-    scheduleYear: { type: Number, required: true },
-    scheduleMonth: { type: Number, required: true },
-    scheduleDay: { type: Number, required: true },
+    scheduleYear: { type: String, required: true },
+    scheduleMonth: { type: String, required: true },
+    scheduleDay: { type: String, required: true },
     scheduleContent: { type: String },
     groupIndex: { type: Number },
     groupName: { type: String },
